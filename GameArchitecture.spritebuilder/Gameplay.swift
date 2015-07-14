1f8f11c2a05d167739867a0b641dbaafacdf0f25
otamm <oivatom@gmail.com>
2015-07-14 03:23:30 -0400

    Created 'Flag' class; added animations to flag & character in SpriteBuilder and made both into physics objects

--- GameArchitecture.spritebuilder/Gameplay.swift
+++ GameArchitecture.spritebuilder/Gameplay.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation;
+
+class Gameplay:CCNode, CCPhysicsCollisionDelegate {
+    /* linked objects */
+    
+    // will contain current level scene.
+    weak var levelNode:CCNode!;
+    
+    // will apply physics on its child nodes
+    weak var gamePhysicsNode:CCPhysicsNode!;
+}
